c68c8165eb132ca2281a520ebb224563e1c0512f
Swati D <swatidurgekar@gmail.com>
2025-08-11 01:05:03 +0530

    Blog page

--- src/app/(landing-page)/components/PostsSection.js
+++ src/app/(landing-page)/components/PostsSection.js
@@ -1,25 +1,43 @@
-import { Link } from "lucide-react";
+import Link from "next/link";
 import { getPayload } from "../../../lib/payload";
+
+function formatDate(timeStamp) {
+    const date = new Date(timeStamp);
+    return date.toLocaleDateString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+    })
+}
 
 const PostsSection = async () => {
     const payload = await getPayload();
     const posts = await payload.find({
         collection: "posts",
-        limit: 2
+        limit: 2,
+        sort: '-createdAt',
     })
     console.log(posts);
 
     return (
-        <div>
-            <h2>Latest from <br /> the blog</h2>
-            <div>
-                {posts.docs.map((post) => (
-                    <Link href={`/posts/${post.slug}`} key={post.id}>
-                        <article>
-                            <h3>{post.title}</h3>
-                        </article>
-                    </Link>
-                ))}
+        <div className="max-w-7xl mx-auto px-4 py-24 sm:px-6 lg:px-8">
+            <div className="flex gap-12 sm:gap-24">
+                <div className="w-72">
+                    <h2 className="text-4xl text-bold text-light">Latest from <br /> the blog</h2>
+                </div>
+                <div className="flex-1 grid grid-cols-2 gap-12">
+                    {posts.docs.map((post) => (
+                        <Link href={`/posts/${post.id}`} key={post.id} className="group block">
+                            <article className="space-y-4">
+                                <h3 className="text-2xl font-semibold text-light group-hover:text-purple-400
+                                transition-colors mb-3">{post.title}</h3>
+                            </article>
+                            <div>
+                                <time className="text-slate-300">{formatDate(post.createdAt)}</time>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     )
